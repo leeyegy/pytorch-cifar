07fe8f979e31a5178fe14967e022081a64b9c715
leeyegy <acmrusher@gmail.com>
2020-10-04 09:38:43 +0800

    by bash file

--- bn_train.py
+++ bn_train.py
@@ -54,6 +54,8 @@ parser.add_argument('--resume_last', default=False, action='store_true',
                     help='resume from checkpoint')
 parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                     help='how many batches to wait before logging training status')
+parser.add_argument('--random_bn', default=False, action='store_true',
+                    help='random bn')
 args = parser.parse_args()
 
 # for wideres
@@ -69,8 +71,8 @@ torch.backends.cudnn.benchmark = True
 
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-save_path = os.path.join("checkpoint","bn_"+args.net,"mart_beta_"+str(args.beta))
-exp_name = os.path.join("runs", "bn_"+args.net,"mart_beta_"+str(args.beta))
+save_path = os.path.join("checkpoint","bn_"+args.net,"mart_beta_"+str(args.beta)) if not args.random_bn else os.path.join("checkpoint","random_bn_"+args.net,"mart_beta_"+str(args.beta))
+exp_name = os.path.join("runs", "bn_"+args.net,"mart_beta_"+str(args.beta)) if not args.random_bn else os.path.join("runs", "random_bn_"+args.net,"mart_beta_"+str(args.beta))
 
 if not os.path.exists(save_path):
     os.makedirs(save_path)
@@ -121,18 +123,25 @@ if device == 'cuda':
     net = torch.nn.DataParallel(net)
 
 checkpoint = torch.load(os.path.join("checkpoint",'clean_ce_res18.pth'))
-net.load_state_dict(checkpoint['net'])
-print("best clean acc: {}".format(best_acc))
+if args.random_bn:
+    model_dict = net.state_dict()
+    pretrained_dict = checkpoint['net']
+    pretrained_dict = {k: v for k, v in pretrained_dict.items() if not ("bn" in k or "linear" in k or "shortcut.1." in k)}
+    model_dict.update(pretrained_dict)
+    net.load_state_dict(model_dict)
+else:
+    net.load_state_dict(checkpoint['net'])
+print("best clean acc: {}".format(checkpoint['acc']))
 
 # fixed param
 for k,v in net.named_parameters():
     if "bn" in k or "linear" in k or "shortcut.1." in k:
         v.requires_grad=False
 
-for k,v in net.named_parameters():
-    if "bn" in k or "linear" in k or "shortcut.1." in k:
-        print(k)
-        print(v.requires_grad)
+# for k,v in net.named_parameters():
+#     if "bn" in k or "linear" in k or "shortcut.1." in k:
+#         print(k)
+#         print(v.requires_grad)
 
 assert not (args.resume_best and args.resume_last)
 
--- bn_train.sh
+++ bn_train.sh
@@ -2,6 +2,6 @@ for beta in 6
 do
 	for net in ResNet18
 	do
-	python bn_train.py --net $net  --beta $beta --epsilon 0.03137 | tee log/bn_training/mart_beta_$beta\_$net\_0.03137.txt
+	python bn_train.py --random_bn --net $net  --beta $beta --epsilon 0.03137 | tee log/bn_training/mart_random_bn_beta_$beta\_$net\_0.03137.txt
 done
 done
